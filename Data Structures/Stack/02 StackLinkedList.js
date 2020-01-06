c849637b9fb5bce312100f8e198efdd30205cef9
Pramod Mali <pramodmaali@gmail.com>
2020-01-06 12:49:39 +0530

    Updated format

--- Data Structures/Stack/02 StackLinkedList.js
+++ Data Structures/Stack/02 StackLinkedList.js
@@ -1,65 +1,76 @@
-class Node{
-	constructor(data){
-			this.value = data;
-			this.next = null;
-	}
+class Node {
+  constructor(data) {
+    this.value = data;
+    this.next = null;
+  }
 }
 
-class LinkedListStack{
-	constructor(){
-		this.head = null;	
-	}
-	
-	push(item){
-		let newNode = new Node(item);
-		if(this.head==null){
-			this.head = newNode
-			return;		
-		}
-		
-		newNode.next = this.head;
-		this.head = newNode;
-	}
-	
-	pop(){
-		if(this.isEmpty()){
-			console.log('Underflow')
-			return;
-		}
-		
-		this.head = this.head.next;
-	}
-	isEmpty(){
-		return this.head==null;
-	}
-	
-	peek(){
-		return this.head?this.head.value:'Stack is empty';
-	}
-	
-	print(){
-		let currentNode = this.head;
-		let printStr = 'Top--> ';
-		while(currentNode){
-			printStr += currentNode.value+" ";
-			currentNode = currentNode.next;
-		}
-		console.log(printStr)
-	}
+class LinkedListStack {
+  constructor() {
+    this.head = null;
+  }
+
+  push(item) {
+    let newNode = new Node(item);
+    if (this.head == null) {
+      this.head = newNode
+      return;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      console.log('Underflow')
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+  isEmpty() {
+    return this.head == null;
+  }
+
+  peek() {
+    return this.head ? this.head.value : 'Stack is empty';
+  }
+
+  deleteStack() {
+    this.head = null;
+  }
+
+  print() {
+    if (this.isEmpty) {
+      console.log('Stack is empty')
+      return;
+    }
+
+    let currentNode = this.head;
+    let printStr = 'Top--> ';
+    while (currentNode) {
+      printStr += currentNode.value + " ";
+      currentNode = currentNode.next;
+    }
+    console.log(printStr)
+  }
 }
 
 let lls = new LinkedListStack();
-console.log('Peek -->'+lls.peek())
-console.log('isEmpty -->'+lls.isEmpty())
+console.log('Peek -->' + lls.peek())
+console.log('isEmpty -->' + lls.isEmpty())
 lls.pop()
 lls.push(10)
 lls.push(20)
 lls.push(30)
-console.log('Peek -->'+lls.peek())
-console.log('isEmpty -->'+lls.isEmpty())
+console.log('Peek -->' + lls.peek())
+console.log('isEmpty -->' + lls.isEmpty())
 lls.print()
 lls.pop()
 lls.pop()
-console.log('Peek -->'+lls.peek())
-console.log('isEmpty -->'+lls.isEmpty())
+console.log('Peek -->' + lls.peek())
+console.log('isEmpty -->' + lls.isEmpty())
+lls.print()
+lls.deleteStack()
 lls.print()
